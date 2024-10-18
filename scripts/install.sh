1b874eb9ed61200ee9d9e6a156d0092fe6348cc7
Ritish Pentapalli <ritishpentapalli4@gmail.com>
2024-10-18 04:24:27 -0600

    Fix install script for macos

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,7 @@ do
 	if [[ $ans == "n" ]];then
 # installs nvm (Node Version Manager)
 		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+		source ~/.zshrc
 
 # download and install Node.js (you may need to restart the terminal)
 		nvm install 20
